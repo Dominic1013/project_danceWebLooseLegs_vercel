23fe31eac463a399e978612c09448104542822b4
ET Huang <yt159357000@gmail.com>
2024-03-06 21:04:54 +0800

    test cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,6 +3,8 @@ import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
+//test
+
 export const signup = async (req, res, next) => {
   //   console.log(req.body);
   const { username, email, password } = req.body;
