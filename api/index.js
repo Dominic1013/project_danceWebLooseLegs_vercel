0e47fe98a539f369589099d24c0cde68c04fa8a0
ET Huang <yt159357000@gmail.com>
2024-03-06 11:36:01 +0800

    fix cors

--- api/index.js
+++ api/index.js
@@ -27,7 +27,14 @@ mongoose
 const app = express();
 
 // middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["https://project-looselegs-v-client.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 // app.use(express.urlencoded());
